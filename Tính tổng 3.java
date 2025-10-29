f6e3f67176e7de827d0e8d008783c74061073d73
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-10-29 09:34:36 +0700

    Create Tính tổng 3.java

---
+++ Tính tổng 3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static long sum(int n){
+        if(n == 1) return 1;
+        return (long)Math.pow(n,3) + sum(n - 1);
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(sum(n));
+    }
+}
