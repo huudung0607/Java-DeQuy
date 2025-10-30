218dc1d684a0c508c6cf1e3683cbf8ece4c17994
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-10-30 10:07:26 +0700

    Create Tìm tổng chữ số của N.java

---
+++ Tìm tổng chữ số của N.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int mod = 1000000007;
+    public static long sum(long n){
+        if(n < 10) return n;
+        return n % 10 + sum(n/10);
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.print(sum(n));
+    }
+}
